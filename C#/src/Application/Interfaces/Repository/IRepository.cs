81d646c5c22c783f7c0aefb83d39e76f05d0915e
Yamkela <yamkela.qhogwana@outlook.com>
2025-07-23 07:27:24 +0200

    Changes

--- C#/src/Application/Interfaces/Repository/IRepository.cs
+++ C#/src/Application/Interfaces/Repository/IRepository.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Application.Interfaces.Repository
+﻿namespace Application.Interfaces.Repository
 {
-    internal interface IRepository
+    public interface IRepository<T> where T : class
     {
+        //creating an entitiy cause I will need to create a user, create a website and also create a record for emails sent and also create a mailing list
+        public Task Create (T entity);
+        //updating a record
+        public Task Update(T entity);
+        //getting a record
+        public Task<T> GetById(object id);
+        public IEnumerable<T> GetAll();
+        //deleting a record as well
+        public Task Delete(T entity);
+        //the purpose of this repository 
     }
 }
