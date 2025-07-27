11ae9199beff557979c1516121b7cc2956686cd8
Yamkela <yamkela.qhogwana@outlook.com>
2025-07-27 14:34:04 +0200

    Temp Fix

--- C#/src/Infrastructure/Repositories/Repository.cs
+++ C#/src/Infrastructure/Repositories/Repository.cs
@@ -7,4 +7,29 @@ public class Repository<T>(DbContext dbContext) : IRepository<T> where T : class
 {
     private readonly DbContext _dbContext = dbContext;
     protected DbSet<T> Entities => _dbContext.Set<T>();
+
+    public Task<T> CreateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> DeleteAsync(object id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T?> GetAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<T> UpdateAsync(T entity)
+    {
+        throw new NotImplementedException();
+    }
 }
